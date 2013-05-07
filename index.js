8d4c7963e000d8ec1676b4af0af831a1ad4b607e
Tim Schumacher <tim@bandenkrieg.hacked.jp>
2013-05-07 18:20:19 +0200

    The script fetches the weatherdata from fh jena page and spits the temperature in celsius.

--- index.js
+++ index.js
@@ -1,3 +1,36 @@
 /*
  * Script to fetch data from http://wetter.mb.fh-jena.de/station/datenbank/php_giese/online.php
  */
+
+
+var http = require('http');
+var cheerio = require('cheerio');
+
+var options = {
+    hostname: 'wetter.mb.fh-jena.de',
+    port: 80,
+    path: '/station/datenbank/php_giese/online.php',
+    method: 'GET'
+};
+
+var data = "";
+
+var req = http.request(options,function(res){
+    res.setEncoding('utf8');
+    res.on('data', function (chunk) {
+        data = data + chunk;
+    });
+
+    res.on('end',function(){
+        var $ = cheerio.load(data);
+        var temp = Number($('strong',$($('td',$($('tr',$($('table')[5]))[1]))[2])).text().split(' ')[0]);
+        console.log(temp);
+    });
+    
+});
+
+req.on('error', function(e) {
+  console.log('problem with request: ' + e.message);
+});
+
+req.end();
